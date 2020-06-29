7f52b822fdd145e8c619ffee2b9f491a0eb02a1b
Patrick Le <patrick.vt.le@gmail.com>
2020-06-29 13:03:30 +1000

    Updated to have Database Context Setup Ready for Usage with SQLite

---
+++ DatingApp.API/Models/Value.cs
@@ -0,0 +1,11 @@
+namespace DatingApp.API.Models
+// Setting up entity to use to get and set required values 
+{
+  public class Value
+  {
+    // Id Value for each model
+    public int Id { get; set; }
+    // Name Value for each model
+    public string Name { get; set; }
+  }
+}
