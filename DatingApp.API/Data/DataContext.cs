dfd9735f0a7894e7122c9d7a303f33bede0164e8
Patrick Le <patrick.vt.le@gmail.com>
2020-06-29 14:27:21 +1000

    Updated Comments DbContext

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -6,6 +6,8 @@ namespace DatingApp.API.Data
   public class DataContext : DbContext
   {
     // Constructor uses DbContext as the base options required for generating itself
+    // Using DbContext extension to call required function to generate Constructor
+    // Creates context needed to access required database later
     public DataContext(DbContextOptions<DataContext> options) : base (options){}
 
     // Telling class about entity (reference where our values are being pulled from)
