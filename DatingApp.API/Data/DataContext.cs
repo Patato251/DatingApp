a87af2c4dcd5323fafb6baf7855904871ff9c537
Patrick.Le25 <patrick.vt.le@gmail.com>
2020-06-30 13:29:18 +1000

    Updated to Add Secondary Database of Users

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -1,17 +1,24 @@
 using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
 
+// Use this file to let EntityFramework know of development of new class in Models
 namespace DatingApp.API.Data
 {
   public class DataContext : DbContext
   {
-    // Constructor uses DbContext as the base options required for generating itself
-    // Using DbContext extension to call required function to generate Constructor
-    // Creates context needed to access required database later
+    /* 
+     * Constructor uses DbContext as the base options required for generating itself
+     * Derives from use of DbContext from Entityframwork core to take required properties
+     * Creates context needed to access required database later
+     */
     public DataContext(DbContextOptions<DataContext> options) : base (options){}
 
     // Telling class about entity (reference where our values are being pulled from)
     // Values == Table name created when scaffolding database
     public DbSet<Value> Values { get; set; } // Values is name for table generated
+
+    public DbSet<User> Users { get; set; } // Table generated to store User data within database
+
+    /* After every addition of a DbSet, need to migrate database to add new table of data*/
   }
 }
