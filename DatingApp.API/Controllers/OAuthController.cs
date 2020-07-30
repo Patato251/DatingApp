a84171668d1bd3bcea3bc0a08d9698a7990a9953
Patrick.Le25 <patrick.vt.le@gmail.com>
2020-07-30 12:18:44 +1000

    Added Test Oauth Controller

---
+++ DatingApp.API/Controllers/OAuthController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+  [AllowAnonymous]
+  [Route("api/[controller]")]
+  [ApiController]
+  public class OAuthController : ControllerBase
+  {
+    public OAuthController() { }
+
+    []
+  }
+}
